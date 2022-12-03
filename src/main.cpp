5b7d819456624ff4f022a79bbc01c2949991edb8
Hpsaturn <hpsaturn@gmail.com>
2022-12-03 16:51:30 +0100

    display primitives refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,14 @@
+#include <Arduino.h>
+
 #include "HTTPClient.h"
 #include "SD_MMC.h"
 #include "WiFi.h"
 #include "Wire.h"
-#include "XL9535_driver.h"
-#include "esp_lcd_panel_io.h"
-#include "esp_lcd_panel_ops.h"
-#include "esp_lcd_panel_rgb.h"
-#include "esp_lcd_panel_vendor.h"
-#include "factory_ui.h"
-#include "img.h"
-#include "lvgl.h"
-#include "pin_config.h"
-#include <Arduino.h>
-
-// #define TOUCH_MODULE_CST820
-#define TOUCH_MODULE_FT3267
-
-#if defined(TOUCH_MODULE_FT3267)
-#include "ft3267.h"
-#elif defined(TOUCH_MODULE_CST820)
-#define TOUCH_MODULES_CST_SELF
-#include "TouchLib.h"
-#endif
-
-typedef struct {
-  uint8_t cmd;
-  uint8_t data[16];
-  uint8_t databytes; // No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
-} lcd_init_cmd_t;
-
-DRAM_ATTR static const lcd_init_cmd_t st_init_cmds[] = {
-    {0xFF, {0x77, 0x01, 0x00, 0x00, 0x10}, 0x05},
-    {0xC0, {0x3b, 0x00}, 0x02},
-    {0xC1, {0x0b, 0x02}, 0x02},
-    {0xC2, {0x07, 0x02}, 0x02},
-    {0xCC, {0x10}, 0x01},
-    {0xCD, {0x08}, 0x01}, // 用565时屏蔽    666打开
-    {0xb0, {0x00, 0x11, 0x16, 0x0e, 0x11, 0x06, 0x05, 0x09, 0x08, 0x21, 0x06, 0x13, 0x10, 0x29, 0x31, 0x18}, 0x10},
-    {0xb1, {0x00, 0x11, 0x16, 0x0e, 0x11, 0x07, 0x05, 0x09, 0x09, 0x21, 0x05, 0x13, 0x11, 0x2a, 0x31, 0x18}, 0x10},
-    {0xFF, {0x77, 0x01, 0x00, 0x00, 0x11}, 0x05},
-    {0xb0, {0x6d}, 0x01},
-    {0xb1, {0x37}, 0x01},
-    {0xb2, {0x81}, 0x01},
-    {0xb3, {0x80}, 0x01},
-    {0xb5, {0x43}, 0x01},
-    {0xb7, {0x85}, 0x01},
-    {0xb8, {0x20}, 0x01},
-    {0xc1, {0x78}, 0x01},
-    {0xc2, {0x78}, 0x01},
-    {0xc3, {0x8c}, 0x01},
-    {0xd0, {0x88}, 0x01},
-    {0xe0, {0x00, 0x00, 0x02}, 0x03},
-    {0xe1, {0x03, 0xa0, 0x00, 0x00, 0x04, 0xa0, 0x00, 0x00, 0x00, 0x20, 0x20}, 0x0b},
-    {0xe2, {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 0x0d},
-    {0xe3, {0x00, 0x00, 0x11, 0x00}, 0x04},
-    {0xe4, {0x22, 0x00}, 0x02},
-    {0xe5, {0x05, 0xec, 0xa0, 0xa0, 0x07, 0xee, 0xa0, 0xa0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 0x10},
-    {0xe6, {0x00, 0x00, 0x11, 0x00}, 0x04},
-    {0xe7, {0x22, 0x00}, 0x02},
-    {0xe8, {0x06, 0xed, 0xa0, 0xa0, 0x08, 0xef, 0xa0, 0xa0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 0x10},
-    {0xeb, {0x00, 0x00, 0x40, 0x40, 0x00, 0x00, 0x00}, 0x07},
-    {0xed, {0xff, 0xff, 0xff, 0xba, 0x0a, 0xbf, 0x45, 0xff, 0xff, 0x54, 0xfb, 0xa0, 0xab, 0xff, 0xff, 0xff}, 0x10},
-    {0xef, {0x10, 0x0d, 0x04, 0x08, 0x3f, 0x1f}, 0x06},
-    {0xFF, {0x77, 0x01, 0x00, 0x00, 0x13}, 0x05},
-    {0xef, {0x08}, 0x01},
-    {0xFF, {0x77, 0x01, 0x00, 0x00, 0x00}, 0x05},
-    {0x36, {0x08}, 0x01},
-    {0x3a, {0x66}, 0x01},
-    {0x11, {0x00}, 0x80},
-    // {0xFF, {0x77, 0x01, 0x00, 0x00, 0x12}, 0x05},
-    // {0xd1, {0x81}, 0x01},
-    // {0xd2, {0x06}, 0x01},
-    {0x29, {0x00}, 0x80},
-    {0, {0}, 0xff}};
-
-XL9535 xl;
-#if defined(TOUCH_MODULE_CST820)
-TouchLib touch(Wire, IIC_SDA_PIN, IIC_SCL_PIN, CTS820_SLAVE_ADDRESS);
-#endif
+#include "display.h"
 
 bool touch_pin_get_int = false;
 void deep_sleep(void);
 void SD_init(void);
-void tft_init(void);
-void lcd_cmd(const uint8_t cmd);
-void lcd_data(const uint8_t *data, int len);
 void wifi_task(void *param);
 
 void scan_iic(void) {
@@ -133,7 +57,6 @@ static void example_lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_
 }
 
 static void lv_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
-
   touch_point_t p = {0};
   if (touch_pin_get_int) {
 #if defined(TOUCH_MODULE_FT3267)
@@ -156,8 +79,8 @@ static void lv_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data
 }
 
 void setup() {
-  static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
-  static lv_disp_drv_t disp_drv;      // contains callback functions
+  static lv_disp_draw_buf_t disp_buf;  // contains internal graphic buffer(s) called draw buffer(s)
+  static lv_disp_drv_t disp_drv;       // contains callback functions
   static lv_indev_drv_t indev_drv;
   // put your setup code here, to run once:
   pinMode(BAT_VOLT_PIN, ANALOG);
@@ -204,7 +127,7 @@ void setup() {
                       .pclk_active_neg = 1,
                   },
           },
-      .data_width = 16, // RGB565 in parallel mode, thus 16bit in width
+      .data_width = 16,  // RGB565 in parallel mode, thus 16bit in width
       .psram_trans_align = 64,
       .hsync_gpio_num = EXAMPLE_PIN_NUM_HSYNC,
       .vsync_gpio_num = EXAMPLE_PIN_NUM_VSYNC,
@@ -238,7 +161,7 @@ void setup() {
       .user_ctx = NULL,
       .flags =
           {
-              .fb_in_psram = 1, // allocate frame buffer in PSRAM
+              .fb_in_psram = 1,  // allocate frame buffer in PSRAM
           },
   };
   ESP_ERROR_CHECK(esp_lcd_new_rgb_panel(&panel_config, &panel_handle));
@@ -294,70 +217,8 @@ void loop() {
   }
 }
 
-void lcd_send_data(uint8_t data) {
-  uint8_t n;
-  for (n = 0; n < 8; n++) {
-    if (data & 0x80)
-      xl.digitalWrite(LCD_SDA_PIN, 1);
-    else
-      xl.digitalWrite(LCD_SDA_PIN, 0);
-
-    data <<= 1;
-    xl.digitalWrite(LCD_CLK_PIN, 0);
-    xl.digitalWrite(LCD_CLK_PIN, 1);
-  }
-}
-
-void lcd_cmd(const uint8_t cmd) {
-  xl.digitalWrite(LCD_CS_PIN, 0);
-  xl.digitalWrite(LCD_SDA_PIN, 0);
-  xl.digitalWrite(LCD_CLK_PIN, 0);
-  xl.digitalWrite(LCD_CLK_PIN, 1);
-  lcd_send_data(cmd);
-  xl.digitalWrite(LCD_CS_PIN, 1);
-}
-
-void lcd_data(const uint8_t *data, int len) {
-  uint32_t i = 0;
-  if (len == 0)
-    return; // no need to send anything
-  do {
-    xl.digitalWrite(LCD_CS_PIN, 0);
-    xl.digitalWrite(LCD_SDA_PIN, 1);
-    xl.digitalWrite(LCD_CLK_PIN, 0);
-    xl.digitalWrite(LCD_CLK_PIN, 1);
-    lcd_send_data(*(data + i));
-    xl.digitalWrite(LCD_CS_PIN, 1);
-    i++;
-  } while (len--);
-}
-
-void tft_init(void) {
-  xl.digitalWrite(LCD_CS_PIN, 1);
-  xl.digitalWrite(LCD_SDA_PIN, 1);
-  xl.digitalWrite(LCD_CLK_PIN, 1);
-
-  // Reset the display
-  xl.digitalWrite(LCD_RST_PIN, 1);
-  vTaskDelay(200 / portTICK_PERIOD_MS);
-  xl.digitalWrite(LCD_RST_PIN, 0);
-  vTaskDelay(200 / portTICK_PERIOD_MS);
-  xl.digitalWrite(LCD_RST_PIN, 1);
-  vTaskDelay(200 / portTICK_PERIOD_MS);
-  int cmd = 0;
-  while (st_init_cmds[cmd].databytes != 0xff) {
-    lcd_cmd(st_init_cmds[cmd].cmd);
-    lcd_data(st_init_cmds[cmd].data, st_init_cmds[cmd].databytes & 0x1F);
-    if (st_init_cmds[cmd].databytes & 0x80) {
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-    cmd++;
-  }
-  Serial.println("Register setup complete");
-}
-
 void SD_init(void) {
-  xl.digitalWrite(SD_CS_PIN, 1); // To use SDIO one-line mode, you need to pull the CS pin high
+  xl.digitalWrite(SD_CS_PIN, 1);  // To use SDIO one-line mode, you need to pull the CS pin high
   SD_MMC.setPins(SD_CLK_PIN, SD_CMD_PIN, SD_D0_PIN);
   if (!SD_MMC.begin("/sdcard", true)) {
     Serial.println("Card Mount Failed");
